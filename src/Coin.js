988412487c50eacf2e953a5ccb0b7ec513e049cf
Pragati Kundu <kundupragati2@gmail.com>
2021-05-11 21:14:36 +0530

    styles update

--- src/Coin.js
+++ src/Coin.js
@@ -5,8 +5,8 @@ class Coin extends Component{
         return(
             <div>
                 {this.props.isHead != null ? 
-                    <img src={this.props.isHead? 'https://qphs.fs.quoracdn.net/main-qimg-1d5a0e61da032da066453daa3fb43b50.webp' : 
-                    'https://qphs.fs.quoracdn.net/main-qimg-148ae81e6fe0500e130fb547026a9b26'} alt=""/>:null }
+                    <img src={this.props.isHead? 'https://en.numista.com/catalogue/photos/inde/1690-180.jpg' : 
+                    'https://en.numista.com/catalogue/photos/inde/3147-180.jpg'} alt=""/>:null }
             </div>
         )
     }
